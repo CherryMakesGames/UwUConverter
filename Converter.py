35278a38602c0850969c747e32d4e8a315c56dc0
CherryMakesGames <dscleper@gmail.com>
2026-08-01 18:33:38 +0200

    added more file conversions

--- Converter.py
+++ Converter.py
@@ -9,12 +9,52 @@ file_types = {
     ".mp4": [
         ("convert to mp3", "Convert To MP3", "MP3"),
         ("convert to wav", "Convert To WAV", "WAV"),
-        ("convert to mkv", "Convert To MKV", "MKV")
+        ("convert to flac", "Convert To FLAC", "FLAC"),
+        ("convert to audio ogg", "Convert To Audio OGG", "OGG"),
+        ("convert to mkv", "Convert To MKV", "MKV"),
+        ("convert to mov", "Convert To MOV", "MOV"),
+        ("convert to avi", "Convert To AVI", "AVI"),
+        ("convert to webm", "Convert To WEBM", "WEBM")
     ],
     ".mkv": [
-        ("convert to mp4", "Convert To MP4", "MP4"),
         ("convert to mp3", "Convert To MP3", "MP3"),
-        ("convert to wav", "Convert To WAV", "WAV")
+        ("convert to flac", "Convert To FLAC", "FLAC"),
+        ("convert to audio ogg", "Convert To Audio OGG", "OGG"),
+        ("convert to wav", "Convert To WAV", "WAV"),
+        ("convert to mp4", "Convert To MP4", "MP4"),
+        ("convert to mov", "Convert To MOV", "MOV"),
+        ("convert to avi", "Convert To AVI", "AVI"),
+        ("convert to webm", "Convert To WEBM", "WEBM")
+    ],
+    ".mov": [
+        ("convert to mp3", "Convert To MP3", "MP3"),
+        ("convert to flac", "Convert To FLAC", "FLAC"),
+        ("convert to audio ogg", "Convert To Audio OGG", "OGG"),
+        ("convert to wav", "Convert To WAV", "WAV"),
+        ("convert to mp4", "Convert To MP4", "MP4"),
+        ("convert to avi", "Convert To AVI", "AVI"),
+        ("convert to webm", "Convert To WEBM", "WEBM"),
+        ("convert to mkv", "Convert To MKV", "MKV")
+    ],
+    ".avi": [
+        ("convert to mp3", "Convert To MP3", "MP3"),
+        ("convert to flac", "Convert To FLAC", "FLAC"),
+        ("convert to audio ogg", "Convert To Audio OGG", "OGG"),
+        ("convert to mp4", "Convert To MP4", "MP4"),
+        ("convert to wav", "Convert To WAV", "WAV"),
+        ("convert to webm", "Convert To WEBM", "WEBM"),
+        ("convert to mkv", "Convert To MKV", "MKV"),
+        ("convert to mov", "Convert To MOV", "MOV")
+    ],
+    ".webm": [
+        ("convert to mp3", "Convert To MP3", "MP3"),
+        ("convert to flac", "Convert To FLAC", "FLAC"),
+        ("convert to audio ogg", "Convert To Audio OGG", "OGG"),
+        ("convert to wav", "Convert To WAV", "WAV"),
+        ("convert to mp4", "Convert To MP4", "MP4"),
+        ("convert to avi", "Convert To AVI", "AVI"),
+        ("convert to mkv", "Convert To MKV", "MKV"),
+        ("convert to mov", "Convert To MOV", "MOV")
     ]
 }
 
@@ -26,7 +66,7 @@ def ConvertFile(file_path, convert_type):
     output_file = av.open(output_file_pre_suffix + '.' + convert_type, 'w')
     try:
         match convert_type.lower():
-            case "mp4" | "mkv":
+            case "mp4" | "mkv" | "webm" | "mov" | "avi":
                 stream_map = {}
 
                 for in_stream in input_file.streams:
