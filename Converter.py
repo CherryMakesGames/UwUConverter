f25c25c04113c9eec8884fa7cd2a5caafe8d3aee
CherryMakesGames <dscleper@gmail.com>
2026-08-01 15:45:03 +0200

    iplemented proper right click menu on files

--- Converter.py
+++ Converter.py
@@ -1,10 +1,17 @@
-import av
 import os
 import sys
+import pathlib
+import av
 import make_key
 
 file_types = {
     ".mp4": [
+        ("convert to mp3", "Convert To MP3", "MP3"),
+        ("convert to wav", "Convert To WAV", "WAV"),
+        ("convert to mkv", "Convert To MKV", "MKV")
+    ],
+    ".mkv": [
+        ("convert to mp4", "Convert To MP4", "MP4"),
         ("convert to mp3", "Convert To MP3", "MP3"),
         ("convert to wav", "Convert To WAV", "WAV")
     ]
@@ -12,6 +19,33 @@ file_types = {
 
 av.logging.set_level(av.logging.VERBOSE)
 
+def ConvertFile(file_path, convert_type):
+    input_file = av.open(file_path)
+    output_file_pre_suffix = file_path.remove_suffix(pathlib.Path(file_path).suffix)
+    output_file = av.open(output_file_pre_suffix + '.' + convert_type, 'w')
+
+    match convert_type.lower():
+        case "mp4":
+            for in_stream in input_file.streams:
+                out_stream = output_file.add_stream(template=in_stream)
+                for packet in input_file.demux(in_stream):
+                    if packet.dts is None:
+                        print("***")
+                        continue
+                    packet.stream = out_stream
+                    output_file.mux(packet)
+            pass
+        case "mp3":
+            
+            pass
+        case _:
+            pass
+    input_file.close()
+    output_file.close()
+
 if __name__ == "__main__":
-    make_key.CreateExtensions(file_types)
+    if len(sys.argv) > 2:
+        ConvertFile(sys.argv[1], sys.argv[2])
+    else: 
+        make_key.CreateExtensions(file_types)
     
--- make_key.py
+++ make_key.py
@@ -13,28 +13,65 @@ path_start = r"Software\\Classes\\SystemFileAssociations\\"
 def CreateExtensions(file_types):
     for extension, conversions in file_types.items():
         ResetExtension(extension)
-        AddExtension(extension)
+        AddExtension(extension, conversions)
+
+def DeleteTree(root, key_path):
+    try:
+        with reg.OpenKey(root, key_path, 0, reg.KEY_READ | reg.KEY_WRITE) as key:
+            while True:
+                try:
+                    child_name = reg.EnumKey(key, 0)
+                    DeleteTree(root, key_path + r"\\" + child_name)
+                except OSError:
+                    break
+
+        reg.DeleteKey(root, key_path)
+
+    except FileNotFoundError:
+        pass
 
 def ResetExtension(file_type):
     key_path = path_start + file_type + name
-
-    try:
-        reg.DeleteKey(
-            reg.HKEY_CURRENT_USER,
-            key_path + r"\\command"
-        )
-    except FileNotFoundError:
-        pass
-
-    try:
-        reg.DeleteKey(
-            reg.HKEY_CURRENT_USER,
-            key_path
-        )
-    except FileNotFoundError:
-        pass
+    DeleteTree(reg.HKEY_CURRENT_USER, key_path)
     
 
-def AddExtension(file_type):
+def AddExtension(file_type, conversions):
     key_path = path_start + file_type + name
-    reg.CreateKey(reg.HKEY_CURRENT_USER, key_path)
+
+    with reg.CreateKey(reg.HKEY_CURRENT_USER, key_path) as key:
+        reg.SetValueEx(key, "MUIVerb", 0, reg.REG_SZ, "Convert With UwUConverter ^-^")
+
+        reg.SetValueEx(
+            key,
+            "SubCommands",
+            0,
+            reg.REG_SZ,
+            ""
+        )
+
+    for conversion_id, menu_text, convert_type in conversions:
+        conversion_path = key_path + r"\\shell\\" + conversion_id
+
+        with reg.CreateKey(reg.HKEY_CURRENT_USER, conversion_path) as conversion_key:
+            reg.SetValueEx(conversion_key, "MUIVerb", 0, reg.REG_SZ, menu_text)
+
+        command_path = conversion_path + r"\\command"
+
+        command = (
+            f'"{python_exe}" '
+            f'"{cwd}\\Converter.py" '
+            f'"%1" '
+            f'"{convert_type}"'
+        )
+
+        with reg.CreateKey(
+            reg.HKEY_CURRENT_USER,
+            command_path
+        ) as command_key:
+            reg.SetValueEx(
+                command_key,
+                "",
+                0,
+                reg.REG_SZ,
+                command
+            )
