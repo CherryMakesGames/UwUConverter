25dae9fd6969cc64537239ddad7debedd855258a
CherryMakesGames <dscleper@gmail.com>
2026-08-02 15:32:42 +0200

    Got all video2video encoding working

--- Converter.py
+++ Converter.py
@@ -142,9 +142,10 @@ def ConvertFile(file_path, convert_type):
                 Remux(input_file, output_file)
                 pass
             case "webm":
-
+                Transcode(input_file, output_file, "vp8", "libopus")
                 pass
             case "avi":
+                Transcode(input_file, output_file, "libsvtav1", "libmp3lame")
                 pass
             case "mp3" | "wav" | "flac" | "ogg":
                 stream_map = {}
@@ -191,25 +192,44 @@ def Remux(input_file, output_file):
         packet.stream = stream_map[packet.stream.index]
         output_file.mux(packet)
 
-def Remux(input_file, output_file):
+def Transcode(input_file, output_file, encoderVideo, encoderAudio):
     stream_map = {}
 
     for in_stream in input_file.streams:
-        if in_stream.type not in ("video", "audio"):
-            continue
+        if in_stream.type == "video":
+            out_stream = output_file.add_stream(
+                encoderVideo,
+                rate=in_stream.average_rate
+            )
 
-        out_stream = output_file.add_stream_from_template(in_stream)
-        stream_map[in_stream.index] = out_stream
+            out_stream.width = in_stream.codec_context.width
+            out_stream.height = in_stream.codec_context.height
+            out_stream.pix_fmt = "yuv420p"
+
+            stream_map[in_stream.index] = out_stream
+
+        elif in_stream.type == "audio":
+            out_stream = output_file.add_stream(
+                encoderAudio,
+                rate=in_stream.codec_context.sample_rate
+            )
+
+            out_stream.layout = in_stream.codec_context.layout
+            stream_map[in_stream.index] = out_stream
 
     for packet in input_file.demux():
-        if packet.dts is None:
-            continue
-
         if packet.stream.index not in stream_map:
             continue
 
-        packet.stream = stream_map[packet.stream.index]
-        output_file.mux(packet)
+        out_stream = stream_map[packet.stream.index]
+
+        for frame in packet.decode():
+            for output_packet in out_stream.encode(frame):
+                output_file.mux(output_packet)
+
+    for out_stream in stream_map.values():
+        for packet in out_stream.encode():
+            output_file.mux(packet)
 
 def RemuxAudio(input_file, output_file):
     stream_map = {}
